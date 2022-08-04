0ad211109094f342c72f1603a3c0ebfb934ecc91
Nathalie Pires Cesarino <nathalie.cesarino@kroton.tech>
2022-08-04 20:43:05 -0300

    Initial Commit

---
+++ Netflix_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Clone
+//
+//  Created by Nathalie Cesarino on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
